8a1951c81a6dc5ba9b986c0d6b4c204eeaa55c59
zhap078 <zhap078@live.rhul.ac.uk>
2020-05-28 08:42:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f=open("Galaxy1.txt","r")
+f.readline()
+Radius=[]
+dR=[]
+Velocity=[]
+dV=[]
+Mass=[]
+G=4.3*(10**-6)
+predictV=[]
+
+for line in f:
+  Radius.append(float(line.split("\t")[0]))
+  dR.append(float(line.split("\t")[2]))
+  Velocity.append(float(line.split("\t")[1]))
+  dV.append(float(line.split("\t")[3]))
+  Mass.append(float(line.split("\t")[4]))
+  predictV.append(((float(G))*(float(line.split("\t")[4]))/(float(line.split("\t")[0])))**(0.5))
+
+x=np.array(Radius)
+y=np.array(Velocity)
+z=np.array(predictV)
+plt.plot(x,y)
+plt.plot(x,z)
+plt.xlabel('Radius (km/h)')
+plt.ylabel('Velocity (kpc)')
+plt.show()
