c4c207319a0f80fffeaae7d331f619d990f026e8
zhap078 <zhap078@live.rhul.ac.uk>
2020-06-07 19:05:38 +0000

    new graph

--- main3.py
+++ main3.py
@@ -14,14 +14,18 @@ predictV=[]
 p=100*(10**6)
 r=1.87
 MassDM=[]
+MaddDM=[]
 
 for line in f:
   Mass.append(float(line.split("\t")[4]))
   MassDM.append(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r))))
+  MaddDM.append((float(line.split("\t")[4]))+(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r)))))
 
 x=np.array(Mass)
 y=np.array(MassDM)
+z=np.array(MaddDN)
 plt.plot(x,y)
+plt.plot(x,z)
 plt.xlabel('Mass (Solar Masses)')
 plt.ylabel('Dark Matter (Solar Masses)')
 plt.show()
