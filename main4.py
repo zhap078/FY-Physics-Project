6f480ec1e6b255bb52d382eae14e921487455928
zhap078 <zhap078@live.rhul.ac.uk>
2020-06-07 17:40:56 +0000

    q12, i need help.

---
+++ main4.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.array([5,10,15,20,25])
+y=np.array([0.2,0.5,0.8,1.0,1.3])
+yerr = np.array([0.05,0.05,0.05,0.05,0.05])
+#the next line is the model prediction,
+#where we make some initial guess
+#what the slope of the line will be
+fx=0.04*x
+plt.errorbar(x,y,yerror,fmt=’bo’)
+plt.plot(x,fx,’r-’)
+plt.show()
