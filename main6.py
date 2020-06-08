18eb79454f859cfded1245ca76500d4d587b4650
zhap078 <zhap078@live.rhul.ac.uk>
2020-06-08 12:36:43 +0000

    comparing another galaxy

--- main6.py
+++ main6.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f=open("Galaxy2.txt","r")
+f=open("Galaxy1.txt","r")
 f.readline()
+
 Radius=[]
 dR=[]
 Velocity=[]
@@ -10,6 +11,17 @@ dV=[]
 Mass=[]
 G=4.3*(10**-6)
 predictV=[]
+
+g=open("Galaxy2.txt","r")
+g.readline()
+
+Radius2=[]
+dR2=[]
+Velocity2=[]
+dV2=[]
+Mass2=[]
+G=4.3*(10**-6)
+predictV2=[]
 
 for line in f:
    Radius.append(float(line.split("\t")[0]))
@@ -19,11 +31,24 @@ for line in f:
    Mass.append(float(line.split("\t")[4]))
    predictV.append(((float(G))*(float(line.split("\t")[4]))/(float(line.split("\t")[0])))**(0.5))
 
+for line in g:
+   Radius2.append(float(line.split("\t")[0]))
+   dR2.append(float(line.split("\t")[2]))
+   Velocity2.append(float(line.split("\t")[1]))
+   dV2.append(float(line.split("\t")[3]))
+   Mass2.append(float(line.split("\t")[4]))
+   predictV2.append(((float(G))*(float(line.split("\t")[4]))/(float(line.split("\t")[0])))**(0.5))
+
 x=np.array(Radius)
 y=np.array(Velocity)
 z=np.array(predictV)
+x2=np.array(Radius2)
+y2=np.array(Velocity2)
+z2=np.array(predictV2)
 plt.plot(x,y)
 plt.plot(x,z)
+plt.plot(x2,y2)
+plt.plot(x2,z2)
 plt.xlabel('Radius (km/h)')
 plt.ylabel('Velocity (kpc)')
 plt.show()
