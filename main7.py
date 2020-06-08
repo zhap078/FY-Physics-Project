c309bc864897fcb574672f493afcc36d22edd4b5
zhap078 <zhap078@live.rhul.ac.uk>
2020-06-08 13:10:43 +0000

    new comparison graphs

--- main7.py
+++ main7.py
@@ -2,8 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-f=open("Galaxy2.txt","r")
+f=open("Galaxy1.txt","r")
 f.readline()
+g=open("Galaxy2.txt","r")
+g.readline()
+
 Radius=[]
 dR=[]
 Velocity=[]
@@ -14,17 +17,34 @@ predictV=[]
 p=100*(10**6)
 r=1.87
 MassDM=[]
+Radius2=[]
+dR2=[]
+Velocity2=[]
+dV2=[]
+Mass2=[]
+predictV2=[]
+MassDM2=[]
 
 for line in f:
   Mass.append(float(line.split("\t")[4]))
   MassDM.append(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r))))
   predictV.append(((float(G))*(float(line.split("\t")[4]))/(float(line.split("\t")[0])))**(0.5))
 
+for line in g:
+  Mass2.append(float(line.split("\t")[4]))
+  MassDM2.append(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r))))
+  predictV2.append(((float(G))*(float(line.split("\t")[4]))/(float(line.split("\t")[0])))**(0.5))
+
 x=np.array(Mass)
 z=np.array(MassDM)
 y=np.array(predictV)
+x2=np.array(Mass2)
+y2=np.array(MassDM)
+z2=np.array(predictV2)
 plt.plot(x,y)
 plt.plot(z,y)
+plt.plot(x2,y2)
+plt.plot(z2,y2)
 plt.xlabel('Mass (Solar Masses)')
 plt.ylabel('Predicted Velocity (km/s)')
 plt.show()
--- main8.py
+++ main8.py
@@ -2,8 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-f=open("Galaxy2.txt","r")
+f=open("Galaxy1.txt","r")
 f.readline()
+g=open("Galaxy2.txt","r")
+g.readline()
+
 Radius=[]
 dR=[]
 Velocity=[]
@@ -15,17 +18,35 @@ p=100*(10**6)
 r=1.87
 MassDM=[]
 MaddDM=[]
+Radius2=[]
+dR2=[]
+Velocity2=[]
+dV2=[]
+Mass2=[]
+predictV2=[]
+MassDM2=[]
+MaddDM2=[]
 
 for line in f:
   Mass.append(float(line.split("\t")[4]))
   MassDM.append(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r))))
   MaddDM.append((float(line.split("\t")[4]))+(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r)))))
 
+for line in g:
+  Mass2.append(float(line.split("\t")[4]))
+  MassDM2.append(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r))))
+  MaddDM2.append((float(line.split("\t")[4]))+(4*math.pi*p*(r**2)*((float(line.split("\t")[0]))-(r*math.atan((float(line.split("\t")[0]))/r)))))
+
 x=np.array(Mass)
 y=np.array(MassDM)
-z=np.array(MaddDN)
+z=np.array(MaddDM)
+x2=np.array(Mass2)
+y2=np.array(MassDM2)
+z2=np.array(MaddDM2)
 plt.plot(x,y)
 plt.plot(x,z)
+plt.plot(x2,y2)
+plt.plot(x2,z2)
 plt.xlabel('Mass (Solar Masses)')
 plt.ylabel('Dark Matter (Solar Masses)')
 plt.show()
